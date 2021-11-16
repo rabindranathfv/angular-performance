418854cecee769529791f4fb06d683d564622e0e
Rabindranath Ferreira <rabindranathucv@gmail.com>
2021-11-16 15:55:24 +0100

    add users and about modules/componentes with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,20 @@ const routes: Routes = [
     redirectTo: '',
     pathMatch: 'full'
   },
+  { 
+    path: 'about', 
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule) 
+  },
+  { 
+    path: 'users', 
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
   {
     
     path: '',
     loadChildren: () => import('./employee-list/employee-list.module').then(m => m.EmployeeListModule)
-  }
+  },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }
 ];
 
 @NgModule({
